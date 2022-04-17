38e5d99bd9e4a0fe6e3b81a7ce8ad209f8843a8f
Lewis <dsapione@gmail.com>
2022-04-17 15:36:16 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-			<Header
-
-			></Header>
+			<Header></Header>
+			<Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+function Footer() {
+
+
+  return (
+		<footer>
+			<FontAwesomeIcon icon="fa-brands fa-github" />
+		</footer> 
+  );
+}
+
+export default Footer;
